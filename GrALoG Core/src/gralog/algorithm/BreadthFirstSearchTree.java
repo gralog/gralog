2691bba90721d16bec3598be07325597493d91b8
Viktor Engelmann <Viktor.Engelmann@TU-Berlin.de>
2016-07-14 13:49:57 +0200

    added (core) algorithms for DFS and BFS Search-Trees

---
+++ GrALoG Core/src/gralog/algorithm/BreadthFirstSearchTree.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gralog.algorithm;
+
+import gralog.structure.*;
+import gralog.progresshandler.ProgressHandler;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayDeque;
+
+/**
+ *
+ * @author viktor
+ */
+@AlgorithmDescription(
+  name="Breadth-First Search-Tree",
+  text="Constructs a Breadth-First Search-Tree",
+  url="https://en.wikipedia.org/wiki/Breadth-first_search"
+)
+public class BreadthFirstSearchTree extends Algorithm
+{
+    public static void BreadthFirstSearch(Vertex start, HashMap<Vertex, Vertex> predecessor, HashMap<Vertex, Edge> edgeFromPredecessor)
+    {
+        ArrayDeque<Vertex> queue = new ArrayDeque<>();
+        queue.addLast(start);
+        predecessor.put(start, null);
+        edgeFromPredecessor.put(start, null);
+        
+        while(!queue.isEmpty())
+        {
+            Vertex v = queue.removeFirst();
+            
+            for(Edge e : v.getConnectedEdges())
+            {
+                if(e.getSource() != v && e.isDirected) // incoming (directed) edge
+                    continue;
+                
+                Vertex other = e.getTarget();
+                if(other == v)
+                    other = e.getSource();
+
+                if(predecessor.containsKey(other)) // successor already in the tree
+                    continue;
+                predecessor.put(other, v);
+                edgeFromPredecessor.put(other, e);
+                queue.addLast(other);
+            }
+        }
+    }
+    
+    public Object Run(Structure s, AlgorithmParameters p, ProgressHandler onprogress)
+    {
+        HashMap<Vertex, Vertex> predecessor = new HashMap<>();
+        HashMap<Vertex, Edge> edgeFromPredecessor = new HashMap<>();
+        Vertex v = (Vertex)s.getVertices().toArray()[0];
+        BreadthFirstSearch(v, predecessor, edgeFromPredecessor);
+        
+        HashSet<Edge> tree = new HashSet<>();
+        tree.addAll(edgeFromPredecessor.values());
+        return tree;
+    }
+    
+}
---
+++ GrALoG Core/src/gralog/algorithm/DepthFirstSearchTree.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gralog.algorithm;
+
+import gralog.structure.*;
+import gralog.progresshandler.ProgressHandler;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ *
+ * @author viktor
+ */
+@AlgorithmDescription(
+  name="Depth-First Search-Tree",
+  text="Constructs a Depth-First Search-Tree",
+  url="https://en.wikipedia.org/wiki/Depth-first_search"
+)
+public class DepthFirstSearchTree extends Algorithm
+{
+    public static void DepthFirstSearch(Vertex v, HashMap<Vertex, Vertex> predecessor, HashMap<Vertex,Edge> edgeFromPredecessor)
+    {
+        for(Edge e : v.getConnectedEdges())
+        {
+            if(e.getSource() != v && e.isDirected) // incoming (directed) edge
+                continue;
+            
+            Vertex other = e.getTarget();
+            if(other == v)
+                other = e.getSource();
+
+            if(predecessor.containsKey(other)) // successor already in the tree
+                continue;
+            
+            predecessor.put(other, v);
+            edgeFromPredecessor.put(other, e);
+            DepthFirstSearch(other, predecessor, edgeFromPredecessor);
+        }
+    }
+    
+    public Object Run(Structure s, AlgorithmParameters p, ProgressHandler onprogress)
+    {
+        HashMap<Vertex, Vertex> predecessor = new HashMap<>();
+        HashMap<Vertex, Edge> edgeFromPredecessor = new HashMap<>();
+        Vertex v = (Vertex)s.getVertices().toArray()[0];
+        predecessor.put(v,null);
+        edgeFromPredecessor.put(v, null);
+        DepthFirstSearch(v, predecessor, edgeFromPredecessor);
+
+        HashSet<Edge> tree = new HashSet<>();
+        tree.addAll(edgeFromPredecessor.values());
+        return tree;
+    }
+    
+}
