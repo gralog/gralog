527c08500c3edab5bb8112a58d18af75affc1b03
Christoph Dittmann <christoph.dittmann@tu-berlin.de>
2016-11-07 18:10:43 +0100

    Add a test for the modal mu calculus parser

---
+++ GrALoG Modal Mu Calculus/test/gralog/modalmucalculus/parser/ModalMuCalculusParserTest.java
@@ -0,0 +1,98 @@
+/*
+ * This file is part of GrALoG FX, Copyright (c) 2016 LaS group, TU Berlin.
+ * License: https://www.gnu.org/licenses/gpl.html GPL version 3 or later.
+ */
+package gralog.modalmucalculus.parser;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class ModalMuCalculusParserTest {
+
+    private void parseAndCompare(String toParse) throws Exception {
+        parseAndCompare(toParse, toParse);
+    }
+
+    private void parseAndCompare(String toParse, String result) throws Exception {
+        ModalMuCalculusParser parser = new ModalMuCalculusParser();
+        assertEquals(result, parser.parseString(toParse).toString());
+    }
+
+    /**
+     * Test that parsing a formula and printing it produces the same (or
+     * equivalent) result.
+     */
+    @Test
+    public void testParse() throws Exception {
+        parseAndCompare("abc");
+        parseAndCompare("P ∧ Q");
+        parseAndCompare("P ∨ Q");
+        parseAndCompare("P ∧ Q ∨ R");
+        parseAndCompare("(P ∧ Q) ∨ R", "P ∧ Q ∨ R");
+        parseAndCompare("P ∧ (Q ∨ R)");
+
+        // Negation
+        parseAndCompare("¬P");
+        parseAndCompare("¬P ∧ Q");
+        parseAndCompare("(¬P) ∧ Q", "¬P ∧ Q");
+        parseAndCompare("¬(P ∧ Q)");
+        parseAndCompare("¬¬P ∨ Q");
+
+        // Box and diamond
+        parseAndCompare("□P");
+        parseAndCompare("□P ∧ Q");
+        parseAndCompare("(□P) ∧ Q", "□P ∧ Q");
+        parseAndCompare("□(P ∧ Q)");
+        parseAndCompare("□□P ∨ Q");
+        parseAndCompare("[abc]P");
+
+        parseAndCompare("◊P");
+        parseAndCompare("◊P ∧ Q");
+        parseAndCompare("(◊P) ∧ Q", "◊P ∧ Q");
+        parseAndCompare("◊(P ∧ Q)");
+        parseAndCompare("◊◊P ∨ Q");
+        parseAndCompare("<abc>P");
+    }
+
+    /**
+     * Test that parsing LaTeX and ASCII notation works as expected.
+     */
+    @Test
+    public void testParseAlternateNotation() throws Exception {
+        // LaTeX notation
+        parseAndCompare("\\neg P", "¬P");
+        parseAndCompare("P \\vee Q", "P ∨ Q");
+        parseAndCompare("P \\wedge Q", "P ∧ Q");
+        parseAndCompare("\\diamond P", "◊P");
+        parseAndCompare("\\box P", "□P");
+        parseAndCompare("\\mu X. P", "μX. P");
+        parseAndCompare("\\nu X. P", "νX. P");
+
+        // ASCII notation
+        parseAndCompare("-P", "¬P");
+        parseAndCompare("~P", "¬P");
+        parseAndCompare("P + Q", "P ∨ Q");
+        parseAndCompare("P * Q", "P ∧ Q");
+        parseAndCompare("<>P", "◊P");
+        parseAndCompare("[]P", "□P");
+        parseAndCompare("mu X. P", "μX. P");
+        parseAndCompare("nu X. P", "νX. P");
+    }
+    /**
+     * Test parsing of fixpoint operators.
+     */
+    @Test
+    public void testParseFixpoints() throws Exception {
+        parseAndCompare("μX. P");
+        parseAndCompare("νX. P");
+        parseAndCompare("μX. P ∧ Q");
+        parseAndCompare("μX. (P ∧ Q)", "μX. P ∧ Q");
+        parseAndCompare("P ∧ μX. Q");
+        parseAndCompare("P ∧ ¬μX. Q ∧ R");
+        parseAndCompare("¬νX. P ∧ μY. Q ∨ R");
+        parseAndCompare("¬(νX. (P ∧ (μY. (Q ∨ R))))", "¬νX. P ∧ μY. Q ∨ R");
+    }
+}
