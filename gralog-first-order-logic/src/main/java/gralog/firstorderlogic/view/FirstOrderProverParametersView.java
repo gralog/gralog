89a9d5fbddae2389fe00fa6ab5f5b52fb4256f79
Christoph Dittmann <christoph.dittmann@tu-berlin.de>
2016-12-15 18:39:38 +0100

    Retrieve recently stored queries

--- gralog-first-order-logic/src/main/java/gralog/firstorderlogic/view/FirstOrderProverParametersView.java
+++ gralog-first-order-logic/src/main/java/gralog/firstorderlogic/view/FirstOrderProverParametersView.java
@@ -4,39 +4,24 @@
  */
 package gralog.firstorderlogic.view;
 
+import gralog.firstorderlogic.algorithm.FirstOrderProver;
+import gralog.gralogfx.RecentQueries;
 import gralog.firstorderlogic.logic.firstorder.parser.FirstOrderSyntaxChecker;
 import gralog.parser.SyntaxChecker;
-import gralog.firstorderlogic.algorithm.FirstOrderProver;
 import gralog.firstorderlogic.algorithm.FirstOrderProverParameters;
-import gralog.firstorderlogic.logic.firstorder.formula.FirstOrderFormula;
-import gralog.firstorderlogic.logic.firstorder.parser.FirstOrderParser;
+import gralog.gralogfx.UIConstants;
 import gralog.gralogfx.views.*;
-import gralog.preferences.Preferences;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.function.Consumer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-
-import javafx.scene.control.Button;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 /**
@@ -46,37 +31,6 @@ import javafx.scene.text.Text;
 public class FirstOrderProverParametersView
     extends GridPaneView<FirstOrderProverParameters> {
 
-    public Set<String> getUniqueSearches() {
-        String searches = Preferences.getString(FirstOrderProver.class, "searches", "");
-        if (searches.isEmpty())
-            return new HashSet<>();
-        return new HashSet<>(Arrays.asList(searches.split("\n")));
-    }
-
-    public Set<String> getUsedVariables(String text) {
-        Set<String> usedVariables = new HashSet<>();
-        if (text.isEmpty())
-            return usedVariables;
-        String[] split = text.split("\\s+");
-        for (String token : split) {
-            int i;
-            boolean found = false;
-            int end = 1;
-            for (i = 0; i < token.length(); i++) {
-                if (!Character.isLetterOrDigit(token.charAt(i))) {
-                    break;
-                } else if (Character.isDigit(token.charAt(i)) && !found) {
-                    end = i;
-                    found = true;
-                }
-            }
-            if (i == token.length()) {
-                usedVariables.add(token.substring(0, end));
-            }
-        }
-        return usedVariables;
-    }
-
     @Override
     public void setObject(FirstOrderProverParameters params,
         Consumer<Boolean> submitPossible) {
@@ -84,28 +38,42 @@ public class FirstOrderProverParametersView
         if (params == null)
             return;
         this.setVgap(8);
-        this.setHgap(10);
+        this.setHgap(UIConstants.HBOX_SPACING);
 
         Label label = new Label("Formula:");
         label.setMinWidth(USE_PREF_SIZE);
         setConstraints(label, 0, 0);
+
+        Label recentQueriesLabel = new Label("Recent queries:");
+        recentQueriesLabel.setMinWidth(USE_PREF_SIZE);
+        setConstraints(recentQueriesLabel, 0, 1);
 
         TextField formulaField = new TextField(params.parameter);
         setConstraints(formulaField, 1, 0);
         formulaField.promptTextProperty().set("Please enter a first-order formula");
         formulaField.setPrefWidth(1000);
 
-        TextArea textArea = new TextArea();
-        textArea.clear();
+        TreeItem root = new TreeItem();
+        for (String query : RecentQueries.get(FirstOrderProver.class))
+            root.getChildren().add(new TreeItem(query));
+        root.setExpanded(true);
+        TreeView recentQueriesList = new TreeView(root);
+        recentQueriesList.setShowRoot(false);
+        setConstraints(recentQueriesList, 1, 1);
 
-        for (String s : getUniqueSearches()) {
-            textArea.appendText(s);
-            textArea.appendText("\n");
-        }
+        // Update the formula when the user selects one of the recent queries.
+        recentQueriesList.getSelectionModel()
+            .selectedItemProperty()
+            .addListener((ObservableValue observable, Object oldValue, Object newValue)
+                -> formulaField.setText((String) ((TreeItem) newValue).getValue())
+            );
 
         Text hint = new Text();
         syntaxCheck(formulaField, hint, submitPossible);
-        setConstraints(hint, 0, 2, 2, 1);
+
+        HBox hints = new HBox(new Text(FirstOrderSyntaxChecker.explanation()), hint);
+        hints.setSpacing(UIConstants.HBOX_SPACING);
+        setConstraints(hints, 1, 3);
 
         params.parameter = formulaField.getText();
         formulaField.textProperty().addListener(e -> {
@@ -113,115 +81,17 @@ public class FirstOrderProverParametersView
             params.parameter = formulaField.getText();
         });
 
-        textArea.setEditable(false);
-        textArea.setWrapText(false);
-
-        setConstraints(textArea, 1, 1);
-        Button load = new Button("load");
-        Button save = new Button("save");
-        Button delete = new Button("delete");
-        Button clear = new Button("clear");
-        Button substitute = new Button("Substitute");
-        VBox vbox = new VBox(5);
-        vbox.getChildren().addAll(load, save, delete, clear, substitute);
-        vbox.setMinWidth(USE_PREF_SIZE);
-        setConstraints(vbox, 4, 1);
-
-        substitute.setOnAction(e -> {
-            if ((textArea.getSelectedText()) != null) {
-                String text = textArea.getSelectedText();
-                String tfText = formulaField.getText();
-
-                FirstOrderFormula phi;
-                try {
-                    phi = FirstOrderParser.parseString(text);
-                    Set<String> usedInFormula = phi.variables();
-                    Set<String> usedInField;
-                    try {
-                        FirstOrderFormula phi2 = FirstOrderParser.parseString(tfText);
-                        usedInField = phi2.variables();
-                    } catch (Exception ex) {
-                        usedInField = getUsedVariables(tfText);
-                    }
-                    HashMap<String, String> replace = new HashMap<>();
-                    String ch = "a";
-                    char c = 'a';
-                    while (usedInField.contains(ch)) {
-                        ch = String.valueOf(++c);
-                    }
-                    int cnt = 1;
-                    for (String s : usedInFormula) {
-                        replace.put(s, ch + String.valueOf(cnt++));
-                    }
-                    String subformula = phi.substitute(replace);
-                    formulaField.setText(tfText + "( " + subformula + ")");
-                } catch (Exception ex) {
-                    Alert alert = new Alert(AlertType.INFORMATION);
-                    alert.setTitle("Information Dialog");
-                    alert.setHeaderText(null);
-                    alert.setContentText("Please select a valid formula to substitute");
-
-                    alert.showAndWait();
-                }
-            }
-        });
-
-        /*load.setOnAction(e -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open Resource File");
-            fileChooser.getExtensionFilters().addAll(
-                    new ExtensionFilter("Text Files", "*.txt"));
-            Stage stage = new Stage();
-            File f = fileChooser.showOpenDialog(stage);
-            if (f != null) {
-                textArea.clear();
-                Set<String> uniqueSearch;
-                try {
-                    uniqueSearch = getUniqueSearches(f);
-                    for (String s : uniqueSearch) {
-                        textArea.appendText(s);
-                        textArea.appendText("\n");
-                    }
-                }
-                catch (Exception ex) {
-                    Logger.getLogger(FirstOrderProverParametersView.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });*/
-        save.setOnAction(e -> {
-            String text = textArea.getText();
-            String fileName = "Formulae" + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()) + ".txt";
-            try (PrintWriter out = new PrintWriter(new BufferedWriter(
-                    new FileWriter(fileName, true)))) {
-                out.println(text);
-            } catch (IOException ex) {
-                Logger.getLogger(FirstOrderProverParametersView.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Information Dialog");
-            alert.setHeaderText(null);
-            alert.setContentText("File saved successfully as " + fileName);
-
-            alert.showAndWait();
-        });
-
-        delete.setOnAction((ActionEvent e) -> {
-            textArea.replaceSelection("");
-        });
-        clear.setOnAction((ActionEvent e) -> {
-            textArea.clear();
-        });
-
         ColumnConstraints textAreaColumn = new ColumnConstraints();
         textAreaColumn.setHgrow(Priority.ALWAYS);
-        getColumnConstraints().addAll(new ColumnConstraints(), textAreaColumn, new ColumnConstraints());
+        getColumnConstraints().addAll(new ColumnConstraints(), textAreaColumn);
 
         RowConstraints textAreaRow = new RowConstraints();
         textAreaRow.setVgrow(Priority.ALWAYS);
+        textAreaRow.setValignment(VPos.TOP);
         getRowConstraints().addAll(new RowConstraints(), textAreaRow, new RowConstraints());
 
-        this.getChildren().addAll(label, formulaField, textArea, vbox, hint);
+        this.getChildren().addAll(label, recentQueriesLabel,
+            formulaField, recentQueriesList, hints);
     }
 
     private void syntaxCheck(TextField valueField, Text hint,
