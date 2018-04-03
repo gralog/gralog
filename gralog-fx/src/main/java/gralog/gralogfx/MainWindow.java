92b9c9c8666d857e0bbb8493521ea7e53da61d2a
Adrian <alic.adrian.berlin@gmail.com>
2018-04-03 16:37:01 +0200

    Test commit

--- gralog-fx/src/main/java/gralog/gralogfx/MainWindow.java
+++ gralog-fx/src/main/java/gralog/gralogfx/MainWindow.java
@@ -1,7 +1,7 @@
 /* This file is part of Gralog, Copyright (c) 2016-2017 LaS group, TU Berlin.
  * License: https://www.gnu.org/licenses/gpl.html GPL version 3 or later. */
 package gralog.gralogfx;
-
+//test
 import gralog.plugins.*;
 import gralog.structure.*;
 import gralog.importfilter.*;
