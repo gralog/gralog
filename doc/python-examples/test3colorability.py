042bb7183e7a55a4cf5edf289b1085a4317e325b
Roman <roman.rabinovich@tu-berlin.de>
2018-10-26 18:14:49 +0200

    test3colorability.py works now

--- doc/python-examples/test3colorability.py
+++ doc/python-examples/test3colorability.py
@@ -26,25 +26,24 @@ from Gralog import *
 
 # We encode variables as follows:
 #
-# R_v -> cantor_pair(0,v.id)
-# B_v -> cantor_pair(1,v.id)
-# G_v -> cantor_pair(2,v.id)
+# R_v -> cantor_pair(0,v.id+1)
+# B_v -> cantor_pair(1,v.id+1)
+# G_v -> cantor_pair(2,v.id+1)
 #
 # where cantor_pair(x,y) = 1/2 * (x+y) * (x+y+1) + y
 # is the Cantor pairing function (a bijection). The inverse is 
-# inv_cantor_pair(z) defined by inv_cantor_pair(z) = (x,y) where
-# y = z - 1/2 * n * (n+1) with n = floor( 1/2 * ( -1 + sqrt(1 + 8z)) )
-# and x = n - y.
+# inv_cantor_pair(z).
 
 def cantor_pair(x,y):
-    return(1/2 * (x+y) * (x+y+1) + y)
+    return( int(((x+y) * (x+y+1))/2.0 + y))
 
 def inv_cantor_pair(z):
-
-    tmp = math.sqrt(1 + 8*z)
-    n = ( -1 + tmp) / 2.0
-    y = int(math.floor(z - (n * (n+1)) / 2.0))
-    x = int(math.floor(n - y))
+    if not isinstance(z,int):
+        return 0
+    w = int(math.floor((math.sqrt(1 + 8*z)-1)/2.0))
+    t = int((w*(w+1))/2.0)
+    y = z - t
+    x = w - y
     return([x,y])
 
 # For easy reading, wrap cantor_pair and inv_cantor_pair.
@@ -59,18 +58,6 @@ def B(v):
 def G(v):
     return(cantor_pair(3,v.getId()+1))
 
-def isR(v):
-    [x,y] = inv_cantor_pair(v.getId()+1)
-    return(x==1)
-
-def isB(v):
-    [x,y] = inv_cantor_pair(v.getId()+1)
-    return(x==2)
-
-def isG(v):
-    [x,y] = inv_cantor_pair(v.getId()+1)
-    return(x==3)
-
 def graph2cnf(g):
 
     cnf = []
@@ -79,16 +66,13 @@ def graph2cnf(g):
 
         # at lest one colour
         cnf.append([R(v), B(v), G(v)])
+#        gPrint("[R(v), B(v), G(v)] = " + str([R(v), B(v), G(v)]))
 
         # at most one colour
         cnf.append([-R(v), -G(v)])
         cnf.append([-R(v), -B(v)])
-
-        cnf.append([-B(v), -R(v)])
         cnf.append([-B(v), -G(v)])
-
-        cnf.append([-G(v), -R(v)])
-        cnf.append([-G(v), -B(v)])
+        
 
     # endpoints of every edge are coloured properly
 
@@ -101,17 +85,35 @@ def graph2cnf(g):
 
     return(cnf)
 
+def int_to_color(i):
+    if isinstance(i,int):
+        if i == 1:
+            return "R"
+        if i == 2:
+            return "B"
+        if i == 3:
+            return "G"
+    return 0        
 
 def human_cnf(cnf):
+#    gPrint("Human:")
     cnf_human = []
     for clause in cnf:
+#        gPrint("clause: " + str(clause))
+        clause_human = []
         for literal in clause:
-            lit = literal
+            letter = ""
+#            gPrint("literal: " + str(literal))
+            variable = literal
             neg = False
             if literal < 0:
                 neg = True
-                lit = -literal
-            [color,id] = inv_cantor_pair(lit)
+                variable = -literal
+#            gPrint("variable: " + str(variable))
+            [color,id] = inv_cantor_pair(variable)
+            id = int(id)
+            id = id - 1
+#            gPrint("color:" + str(color) + "; id: " + str(id))
             if (color == 1):
                 letter = "R"
             if (color == 2):
@@ -120,31 +122,46 @@ def human_cnf(cnf):
                 letter = "G"
             if neg:
                 letter = "-" + letter
-            cnf_human.append(letter + str(id))
+#            gPrint("translated literal: " + letter + str(id) )
+            clause_human.append(letter + str(id))
+#        gPrint("translated clause: " + str(clause_human) )
+        cnf_human.append(clause_human)
     return(cnf_human)
 
 ###### MAIN #####
 
 g = Graph(None)
-
+vertices = g.getVertices()
+id_to_vertex = dict()
+for v in vertices:
+    id_to_vertex[v.getId()] = v
 
 cnf = graph2cnf(g)
 
-gPrint("Computed cnf:")
-gPrint(str(cnf))
-gPrint("Human view:")
-gPrint([[%s] % (',').join(clause) for clause in human_cnf(cnf)])
-gPrint(','.join(human_cnf(cnf)))
+#gPrint("Computed cnf:")
+#gPrint(str(cnf))
+gPrint("CNF:")
+gPrint("["
+       + '; '.join([ "[%s]" % ','.join(clause) for clause in human_cnf(cnf)])
+       + "]")
+
+gPrint("______")
 
 solution = pycosat.solve(cnf)
 
-gPrint("Solution: "+ solution)
+#gPrint("Solution: "+ str(solution))
+
+
 
 if isinstance(solution,list):   # not UNSAT or UNNKOWN
+    gPrint("Solution:")
     for variable in solution:
         if int(variable) > 0:
             [color,vertexId] = inv_cantor_pair(int(variable))
-            v = g.getVertexById(vertexId)
+            vertexId = int(vertexId) - 1
+            gPrint(int_to_color(color) + str(vertexId))
+#            gPrint("variable: " + str(variable) + "; color:" + str(color) + "; id: " + str(vertexId))
+            v = id_to_vertex[vertexId]         
             if color == 1:
                 v.setColor("Red")
                 continue
