1f52ac2db047cf4adbd0689defdd244dec44a0ec
Roman <roman.rabinovich@tu-berlin.de>
2018-11-16 19:48:00 +0100

    better options for the installer with help

--- install/fncts.sh
+++ install/fncts.sh
@@ -3,92 +3,71 @@
 read_arguments()
 {
 
-    if [ "$#" -ge 3 ]; then
-	echo Too many parameters.
-	print_usage
-	exit 1
-    fi
+    # defaults
 
-
-    DO_INSTALL_PYTHON=1
     DEST_DIR="$HOME/gralog/" # where to install Gralog
-  
-    if [ $# == 0 ]; then
-	# DEST_DIR remains default, install python packages to the system directory (default)
-	PYTHON_LIB_DIR="$(python -c 'import site; print(site.getsitepackages()[0])')"    
-	PYTHON_TO_SYSTEM=1
-    elif [ $# == 1 ]; then
-	parameter=$1
-	if [ ${parameter:0:2} == '-u' ]; then
-	    # DEST_DIR remains default, install python packages to the user directory
-	    PYTHON_LIB_DIR="$(python -m site --user-site)"
-	    PYTHON_TO_SYSTEM=0
-	elif [ ${parameter:0:2} == '-n' ]; then
-	    # DEST_DIR remains default, do not install python packages
-	    DO_INSTALL_PYTHON=0
-	elif [ ${parameter:0:1} == '-' ]; then
-	    echo "Wrong parameters."
-	    print_usage
-	    exit 1
-	else
-	    # The parameter is DEST_DIR, install python packages to the system directory (default)
-	    PYTHON_LIB_DIR="$(python -c 'import site; print(site.getsitepackages()[0])')"    
-	    PYTHON_TO_SYSTEM=1
-	    DEST_DIR="$1"gralog
-	fi
-    else
-	# two parameters
-	first="$1"
-	second="$2"
-	if [ ${first:0:1} == "-" ]; then
-	    # should be -u or -n
-	    # DEST_DIR is the second parameter
-	    DEST_DIR="$2"gralog
-	    if [ ${first:0:2} == "-u" ]; then
-		# install python packages to the user directory
-		PYTHON_LIB_DIR="$(python -m site --user-site)"
-		PYTHON_TO_SYSTEM=0
-	    elif [ ${first:0:2} == "-n" ]; then
-		DO_INSTALL_PYTHON=0
-	    else
-		echo "Wrong parameters."
-		print_usage
-		exit 1
-	    fi
-	else
-	    # The first parameter is DEST_DIR
-	    DEST_DIR="$1"gralog
-	    # The second should be "-u" or "-n"
-	    if [ ${second:0:2} == "-u" ]; then
-		# install python packages to the user directory
-		PYTHON_LIB_DIR="$(python -m site --user-site)"
-		PYTHON_TO_SYSTEM=0
-	    elif [ ${second:0:2} == "-n" ]; then
-		DO_INSTALL_PYTHON=0
-	    else
-		echo "Wrong parameters."
-		print_usage
-		exit 1
-	    fi
-	fi
-    fi
+    DO_MAKE_LINK=0 # make Gralog be available from comand line
+    COMPILE=0 # compile Gralog before installing or not (default: install precompiled binaries)
+    DO_INSTALL_PYTHON=1 # install Python part
+                        # (Gralog.py and graph packages networkx and igraph used in Gralog.py)
+    PYTHON_LIB_DIR="$(python -m site --user-site)" # 
 
-    echo Destination directory: $DEST_DIR
+    
+    # end defaults
 
+    while [ "$1" != "" ]; do
+	case $1 in
+	    -s | --system )         PYTHON_LIB_DIR=\
+			    			  "$(python -c 'import site; \
+					      	  print(site.getsitepackages()[0])')"
+				    PYTHON_TO_SYSTEM=1
+				    ;;
+	    -c | --compile )        COMPILE=1
+				    ;;
+	    -n | --no-python )      DO_INSTALL_PYTHON=0
+				    ;;
+	    -d | --gralog-dir )     shift
+				    DEST_DIR="$1"
+				    ;;
+	    -l | --make-link )      DO_MAKE_LINK=1
+				    ;;
+	    -h | --help )           print_usage
+				    exit 0
+				    ;;
+	    * )                     print_usage
+                                    exit 1
+	esac
+	shift 		
+    done
+    print_arguments
 }
 
 
+print_arguments()
+{
+    echo
+    echo "Install Gralog to: " "$DEST_DIR"
+    echo "Make Gralog available from command line: " $DO_MAKE_LINK
+    echo "Compile before installing:" $COMPILE
+    echo "Install python packages: " $DO_INSTALL_PYTHON
+    echo "Install Gralog.py to: " $PYTHON_LIB_DIR
+    echo
+}
 
 
 print_usage()
 {
-    echo Usage:
-    echo "./install-linux.sh [<path>][ -u| -n]"
-    echo "        <path>: where to install Gralog"
-    echo "        -u: install python packages in user directory"
-    echo "        -n: install python packages in system directory"
     echo
-    echo "Python modules are needed to use gralog with external python scripts, see the Gralog manual."
+    echo Usage:
+    echo "./install-linux.sh [OPTIONS]"
+    echo "OPTIONS:"
+    echo "        -d|--gralog-dir <path>: where to install Gralog (default: ~/gralog/)"
+    echo "        -l|--make-link: make Gralog available from command line (needs sudo, default: no)"
+    echo "        -c|--compile: compile Gralog before installing (default: install binaries)"
+    echo "        -n|--no-python: do not install python packages (default: install them)"
+    echo "        -s|--system: install python packages in system directory (default: in user directory, needs sudo)"
+    echo
+    echo "Python modules are needed to use gralog with external python scripts, see the Gralog manual in doc/manual/main.pdf."
 }
 
 
@@ -126,20 +105,31 @@ install_gralog()
 
     # building Gralog
 
-    echo "Building Gralog..."
     cd ..
-    if ! [ "$(./gradlew build)" ]; then
-	print_error "An error occurred during the compilation and linking of Gralog. Stopping. "
+    if [ $COMPILE = 1 ]; then
+      echo "Building Gralog..."
+      if ! [ "$(./gradlew assemble)" ]; then
+	  print_error "An error occurred during the compilation and linking of Gralog. Stopping. "
+	  exit 1
+      fi    
+      echo "Built Gralog."
+      
+      if ! [ -d build/dist ]; then
+	  echo "Compiling Gralog failed, terminating."
+	  exit 1
+      fi
+    fi
+    if ! [ -d "build/dist" ]; then
+	echo "Directory build/dist does not exist, try to compile Gralog (install with option -c)."
 	exit 1
-    fi    
-    echo "Built Gralog."
-    
-    if ! [ -d build/dist ]; then
-	echo "Compiling Gralog failed, terminating."
+    fi
+
+    if [ -z "$(ls -A build/dist)" ]; then
+	echo "There are no binaries in build/dist, try to compile Gralog (install with option -c)."
 	exit 1
     fi
     cd build/dist
-
+    
 
     # wrapping jar and libraries
 
@@ -165,70 +155,71 @@ install_gralog()
     fi
 
     # symbolic link to wrapper script
-    echo Making symbolic link in /usr/bin...
-    if [ -L /usr/bin/gralog ]; then
-	sudo unlink /usr/bin/gralog 
+    if [ $DO_MAKE_LINK = 1 ]; then
+      echo Making symbolic link in /usr/bin...
+      if [ -L /usr/bin/gralog ]; then
+	  sudo unlink /usr/bin/gralog 
+      fi
+      echo "$DEST_DIR"gralog
+      sudo ln -s "$DEST_DIR"/gralog /usr/bin/gralog
+      echo Done.
     fi
-    echo "$DEST_DIR"gralog
-    sudo ln -s "$DEST_DIR"/gralog /usr/bin/gralog
-    echo Done.
 }
 
 
 install_python_part()
 {
-    if [ ! "$(type -p python)" ]; then
-	print_error "No python in PATH."
-	exit 1
-    fi
-    
-
-
-
-    if [ ! "$(type -p pip)" ]; then
-	echo "Installing pip..."
-	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-	python get-pip.py
-    fi
-
-    if [ ! "$(type -p pip)" ]; then
-	print_error "Could not install pip. Please, install pip and repeat."
-	exit 1
-    else
-	echo "Installed pip."
-    fi
-    
-    if [ "$PYTHON_TO_SYSTEM" ]; then
-	echo "Installing python module \"python-igraph\" to a system directory..."
-	sudo pip install python-igraph
-	echo "\"python-igraph\" installed."
-	echo "Installing python module \"networkx\" to a user directory..."
-	sudo pip install networkx
-	echo "\"networkx\" installed."
-    else
-	echo "Installing python module \"python-igraph\" to a user directory..."
-	pip install --user python-igraph
-	echo "\"python-igraph\" installed."
-	echo "Installing python module \"networkx\" to a user directory..."
-	pip install --user networkx
-	echo "\"networkx\" installed."
-    fi
-
-    # install Gralog.py #
-
-    cd ../..
-    
-    if [ "$PYTHON_TO_SYSTEM" ]; then
-	if [ -L "$PYTHON_LIB_DIR/Gralog.py" ]; then
-	    sudo unlink $PYTHON_LIB_DIR/Gralog.py
+    if [ $DO_INSTALL_PYTHON = 1 ]; then
+	if [ ! "$(type -p python)" ]; then
+	    print_error "No python in PATH."
+	    exit 1
 	fi
-	echo Installing Gralog.py to $PYTHON_LIB_DIR
-	sudo cp -f gralog-fx/src/main/java/gralog/gralogfx/piping/scripts/Gralog.py "$PYTHON_LIB_DIR"/Gralog.py
-    else
-	if [ -L "$PYTHON_LIB_DIR/Gralog.py" ]; then
-	    unlink $PYTHON_LIB_DIR/Gralog.py
+    
+	if [ ! "$(type -p pip)" ]; then
+	    echo "Installing pip..."
+	    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	    python get-pip.py
 	fi
-	echo Installing Gralog.py to $PYTHON_LIB_DIR
-	cp -f gralog-fx/src/main/java/gralog/gralogfx/piping/scripts/Gralog.py "$PYTHON_LIB_DIR"/Gralog.py
+
+	if [ ! "$(type -p pip)" ]; then
+	    print_error "Could not install pip. Please, install pip and repeat."
+	    exit 1
+	else
+	    echo "Installed pip."
+	fi
+	
+	if [ "$PYTHON_TO_SYSTEM" ]; then
+	    echo "Installing python module \"python-igraph\" to a system directory..."
+	    sudo pip install python-igraph
+	    echo "\"python-igraph\" installed."
+	    echo "Installing python module \"networkx\" to a user directory..."
+	    sudo pip install networkx
+	    echo "\"networkx\" installed."
+	else
+	    echo "Installing python module \"python-igraph\" to a user directory..."
+	    pip install --user python-igraph
+	    echo "\"python-igraph\" installed."
+	    echo "Installing python module \"networkx\" to a user directory..."
+	    pip install --user networkx
+	    echo "\"networkx\" installed."
+	fi
+
+	# install Gralog.py #
+
+	cd ../..
+	
+	if [ "$PYTHON_TO_SYSTEM" ]; then
+	    if [ -L "$PYTHON_LIB_DIR/Gralog.py" ]; then
+		sudo unlink $PYTHON_LIB_DIR/Gralog.py
+	    fi
+	    echo Installing Gralog.py to $PYTHON_LIB_DIR
+	    sudo cp -f gralog-fx/src/main/java/gralog/gralogfx/piping/scripts/Gralog.py "$PYTHON_LIB_DIR"/Gralog.py
+	else
+	    if [ -L "$PYTHON_LIB_DIR/Gralog.py" ]; then
+		unlink $PYTHON_LIB_DIR/Gralog.py
+	    fi
+	    echo Installing Gralog.py to $PYTHON_LIB_DIR
+	    cp -f gralog-fx/src/main/java/gralog/gralogfx/piping/scripts/Gralog.py "$PYTHON_LIB_DIR"/Gralog.py
+	fi
     fi
 }
