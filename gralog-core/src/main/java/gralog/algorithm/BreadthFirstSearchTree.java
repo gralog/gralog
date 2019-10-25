b422e6ec174555def9708046e59f0b41b11213f5
RWR <roman.rabinovich@tu-berlin.de>
2019-10-25 20:03:59 +0200

    Rewrote BFS to allow good parameter inout
    Now if in the selection not exactly one vertex is selected, an alert is shown and nothing else done.
    Otherwise BFS from the unique selected vertex is computed. Selected edges do not matter.

--- gralog-core/src/main/java/gralog/algorithm/BreadthFirstSearchTree.java
+++ gralog-core/src/main/java/gralog/algorithm/BreadthFirstSearchTree.java
@@ -50,17 +50,35 @@ public class BreadthFirstSearchTree extends Algorithm {
         }
     }
 
+    /**
+     * If there is a unique vertex among the selected objects, return it.
+     * Otherwise return null.
+     * @param selection
+     * @return
+     */
+    private Vertex selectedUniqueVertex(Set<Object> selection){
+        boolean foundSelectedVertices = false;
+        Vertex v = null;
+        for (Object o: selection)
+            if (o instanceof Vertex) {
+                if (foundSelectedVertices)
+                    return null;
+                else{
+                    foundSelectedVertices = true;
+                    v = (Vertex) o;
+                }
+            }
+        return v;
+    }
+
     public Object run(Structure s, AlgorithmParameters p, Set<Object> selection,
                       ProgressHandler onprogress) {
         HashMap<Vertex, Vertex> predecessor = new HashMap<>();
         HashMap<Vertex, Edge> edgeFromPredecessor = new HashMap<>();
-        Vertex v = null;
-        if (selection != null)
-            for (Object o : selection)
-                if (o instanceof Vertex)
-                    v = (Vertex) o;
+        Vertex v = selectedUniqueVertex(selection);
+
         if (v == null)
-            v = (Vertex) ((s.getVertices().toArray())[0]);
+            return "Please, select exactly one vertex to start BFS from.";
         breadthFirstSearch(v, predecessor, edgeFromPredecessor);
 
         HashSet<Edge> tree = new HashSet<>();
